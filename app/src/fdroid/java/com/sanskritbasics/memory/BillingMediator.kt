1e089ad74bad06d6a4b5b2940fada9174a432cf3
sanskritbscs <sanskritbscs@gmail.com>
2020-12-10 13:31:14 +0300

    fix billing mediator

--- app/src/fdroid/java/com/sanskritbasics/memory/BillingMediator.kt
+++ app/src/fdroid/java/com/sanskritbasics/memory/BillingMediator.kt
@@ -7,14 +7,8 @@ import android.widget.SpinnerAdapter
 
 
 class BillingMediator {
-
-	val isAvailable = false
-
 	fun setParams(cont :Context, view :View) {}
-
 	fun getAdapter() :SpinnerAdapter { return null as SpinnerAdapter }
-
 	fun init( func :(Boolean) -> Any ) { func(false) }
-
 	fun launchBillingFlow(activity :Activity, selectedItemPosition :Int) {}
 }
